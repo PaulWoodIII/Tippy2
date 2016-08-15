ec369630008ec79b7c17d556a6a03290425a671b
Paul Wood III <paul.wood.iii@gmail.com>
2016-08-15 12:25:04 -0400

    Third Tippy

--- Tippy/ViewController.swift
+++ Tippy/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var amountTextField: UITextField!
+    
+    @IBOutlet weak var satisfactionLabel: UILabel!
+    
+    @IBOutlet weak var satisfactionSlider: UISlider!
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +31,52 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    // "😲😭😕😶🙂😀😍"
+    @IBAction func satisfactionSliderValueChanged(sender: AnyObject) {
+        
+        updateViews()
+    }
 
-
+    func updateViews(){
+        
+        var sat = satisfactionSlider.value
+        var satisfactionText = "😶"
+        var tip = 0.0
+        
+        if sat < 0.15 {
+            satisfactionText = "😲"
+            tip = 0.0
+        }
+        else if sat < 0.30 {
+            satisfactionText = "😭"
+            tip = 0.01
+        }
+        else if sat < 0.45 {
+            satisfactionText = "😕"
+            tip = 0.08
+        }
+        else if sat < 0.60 {
+            satisfactionText = "😶"
+            tip = 0.12
+        }
+        else if sat < 0.75 {
+            satisfactionText = "🙂"
+            tip = 0.15
+        }
+        else if sat < 0.90 {
+            satisfactionText = "😀"
+            tip = 0.18
+        }
+        else {
+            satisfactionText = "😍"
+            tip = 0.5
+        }
+        
+        satisfactionLabel.text = satisfactionText
+        var amount = Double(amountTextField.text!)!
+        tipLabel.text = "\(amount * tip)"
+        totalLabel.text = "\( (amount * tip) + amount )"
+    }
+    
 }
 
